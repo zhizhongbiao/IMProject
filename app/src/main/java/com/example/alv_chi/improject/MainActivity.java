12cabf9bb4f01532d9fb286e5b63e8bd0126baba
zhizhongbiao <zhizhongbiao163@163.com>
2017-01-11 12:41:51 +0800

    Initial commit

---
+++ app/src/main/java/com/example/alv_chi/improject/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.alv_chi.improject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
